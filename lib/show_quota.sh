12c59ff6433a2182b10e228f16386f918940c178
Eisphoenix <lukas.frias.santos@gmail.com>
2015-12-11 15:49:49 +0100

    Resolved Problems

--- lib/show_quota.sh
+++ lib/show_quota.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 . /etc/clientcustomizations/lib/log.sh
 
-user="$(whoami)"
+theUser="$(whoami)"
 groups="$(id)"
-groups1="${gruppen##*gid=}"
-groups2="${gruppen1#*(}"
-group="${gruppen2%%)*}"
+groups1="${groups##*gid=}"
+groups2="${groups1#*(}"
+group="${groups2%%)*}"
 used=0
 max=0
 
 if [ "$group" == "teachers" ]
 then
-	used=$(cat "/home/teachers/${user}/Home_auf_Server/.userQuota" | awk '{print int($1)}')
-	max=$(cat "/home/teachers/${user}/Home_auf_Server/.userQuota" | awk '{print int($2)}')
+	used=$(cat "/home/teachers/${theUser}/Home_auf_Server/.userQuota" | awk '{print int($1)}')
+	max=$(cat "/home/teachers/${theUser}/Home_auf_Server/.userQuota" | awk '{print int($2)}')
 else
-	used=$(cat "/home/students/${group}/${user}/Home_auf_Server/.userQuota" | awk '{print int($1)}')
-	max=$(cat "/home/students/${group}/${user}/Home_auf_Server/.userQuota" | awk '{print int($2)}')
+	used=$(cat "/home/students/${group}/${theUser}/Home_auf_Server/.userQuota" | awk '{print int($1)}')
+	max=$(cat "/home/students/${group}/${theUser}/Home_auf_Server/.userQuota" | awk '{print int($2)}')
 fi
 userQuota=$(awk "BEGIN {printf \"%.1f\n\", ${used}/${max}*100}")
 if [[ $userQuota > 80 ]]
@@ -24,4 +24,4 @@ then
 else
 	notify-send -t 10000 -i "/usr/share/icons/gnome/256x256/devices/usbpendrive_unmount.png" "Sie haben ${userQuota}% ihres Speicherplatzes belegt!"
 fi
-log "Showed Quota, ${userQuota}% on User ${user}"
+log "Showed Quota, ${userQuota}% on User ${theUser}"
