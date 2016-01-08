a4e8e97b968d71d47574f4faceea83dd90f25cea
Eisphoenix <lukas.frias.santos@gmail.com>
2016-01-08 17:24:52 +0100

    feat(DynamicTipp): Added dynamic TippOfTheDay, configuration is tippdesTages.csv

--- scripts/tipp-des-tages.sh
+++ scripts/tipp-des-tages.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 . /etc/clientcustomizations/lib/log.sh
 
-notify-send -t 0 -i /etc/clientcustomizations/images/logo.png "Tipp des Tages" "Durch Drücken auf das WG-Hilfe-Symbol kommst du zu den Hilfe-Seiten unseres Systems mit vielen nützlichen Tipps."
+INPUT="../tippdesTages.csv"
+OLDIFS=$IFS
+IFS=\;
+images=()
+texts=()
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read image txt
+do
+	images+=("$image")
+	texts+=("$txt")
+done < $INPUT
+IFS=$OLDIFS
+tipp=$(($RANDOM % ${#images[@]}))
+
+notify-send  -t 10000 -i "/etc/clientcustomizations/images/${images[$tipp]}" "Tipp des Tages" "${texts[$tipp]}"
 
 log "Tipp des Tages successfully executed"
