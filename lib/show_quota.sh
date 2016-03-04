30577c337777eb45cbb1bb645c5d3746c7b4c568
Eisphoenix <lukas.frias.santos@gmail.com>
2016-03-04 14:08:27 +0100

    fix(quota): Fix 8.8% warning
    fix(screensaver): Added sleep

--- lib/show_quota.sh
+++ lib/show_quota.sh
@@ -17,11 +17,12 @@ else
 	used=$(cat "/home/students/${group}/${theUser}/Home_auf_Server/.userQuota" | awk '{print int($1)}')
 	max=$(cat "/home/students/${group}/${theUser}/Home_auf_Server/.userQuota" | awk '{print int($2)}')
 fi
-userQuota=$(awk "BEGIN {printf \"%.1f\n\", ${used}/${max}*100}")
-if [[ $userQuota > 80 ]]
+userQuota=$(awk "BEGIN {printf \"%.1f\n\", ${used}/${max}}")
+userQuotaAsPercent=$(awk "BEGIN {printf \"%.1f\n\", ${used}/${max}*100}")
+if [[ $userQuota > 0.80 ]]
 then
-	notify-send -t 0 -i "/usr/share/icons/gnome/256x256/status/dialog-warning.png" "Achtung!" "Sie haben schon ${userQuota}% ihres Speicherplatzes belegt!"
+	notify-send -t 0 -i "/usr/share/icons/gnome/256x256/status/dialog-warning.png" "Achtung!" "Sie haben schon ${userQuotaAsPercent}% ihres Speicherplatzes belegt!"
 else
-	notify-send -t 10000 -i "/usr/share/icons/gnome/256x256/devices/usbpendrive_unmount.png" "Sie haben ${userQuota}% ihres Speicherplatzes belegt!"
+	notify-send -t 10000 -i "/usr/share/icons/gnome/256x256/devices/usbpendrive_unmount.png" "Sie haben ${userQuotaAsPercent}% ihres Speicherplatzes belegt!"
 fi
 log "Showed Quota, ${userQuota}% on User ${theUser}"
---
+++ scripts/bildschirmschoner_sleep.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /etc/clientcustomizations/lib/log.sh
+
+sh -c "sleep 20; /etc/clientcustomizations/lib/bildschirmschoner_deaktivieren.sh"
